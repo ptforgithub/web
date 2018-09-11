caeb204f6c0c6d3957b7aeee6ee5d428f49f4c77
ptforgithub <ptforgithub@gmail.com>
2018-09-11 11:22:40 +0530

    Export a class

--- WebpackBabel/src/app.js
+++ WebpackBabel/src/app.js
@@ -1,4 +1,9 @@
 import { person } from "./lib";
+import { FirstClass } from "./lib";
+
+var firstClass = new FirstClass("John");
+firstClass.func1();
+
 console.log(person.name);
 
 let test = () => console.log(123);
--- WebpackBabel/src/lib.js
+++ WebpackBabel/src/lib.js
@@ -3,3 +3,12 @@ export const person = {
   location: "Miami",
   age: 30
 };
+
+export class FirstClass {
+  constructor(name) {
+    this.name = name;
+  }
+  func1() {
+    console.log("Hello, " + this.name);
+  }
+}
