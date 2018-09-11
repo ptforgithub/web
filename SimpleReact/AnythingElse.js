36d30f7bcfb41759d2d6a91832b6769690005a1d
ptforgithub <ptforgithub@gmail.com>
2018-09-11 13:13:06 +0530

    Simple React Example with no npm and Component defined in its own file

---
+++ SimpleReact/AnythingElse.js
@@ -0,0 +1,3 @@
+// import ReactDOM from "react";
+import Component01 from "./Component01.js";
+console.log(Component01);
---
+++ SimpleReact/Component01.js
@@ -0,0 +1,10 @@
+class Component01 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>This is a Component</h3>
+        <p>This is a paragraph in the component</p>
+      </div>
+    );
+  }
+}
